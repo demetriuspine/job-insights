07a979c8cd67647ffcf9af1bd8bf2b314164daf9
lucas <mr.pine97@gmail.com>
2022-07-01 17:09:56 -0300

    req01 - impl: implementa a função read

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,7 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, encoding="utf-8") as file:
+      raw_dict = csv.DictReader(file, delimiter=",", quotechar='"')
+      job_list = list(raw_dict)
+      return list(job_list)
