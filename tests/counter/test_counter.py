f8a083eb45c9c3c55f512539a0c8374ee82088f3
lucas <mr.pine97@gmail.com>
2022-07-04 17:11:54 -0300

    req10 - fix: remove linhas em branco

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -5,5 +5,3 @@ def test_counter():
     assert count_ocurrences('src/jobs.csv', 'year') == 5431
     assert count_ocurrences('src/jobs.csv', 'Year') == 5431
     assert count_ocurrences('src/jobs.csv', 'demetrius') == 0
-
-
