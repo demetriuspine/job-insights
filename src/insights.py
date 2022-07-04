c5a56d4689be216702cad229d902baf2326c6f3f
lucas <mr.pine97@gmail.com>
2022-07-04 16:42:48 -0300

    req08 - feat: implementa funcao matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -160,7 +160,15 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if ("min_salary" or "max_salary") not in job:
+        raise ValueError('Either "min_salary" or "max_salary" is missing')
+    elif type(job["min_salary"] or job["max_salary"]) != int:
+        raise ValueError('Both salaries must be an integer')
+    elif job["min_salary"] > job["max_salary"]:
+        raise ValueError('"max_salary" must be greater than "min_salary"')
+    elif type(salary) != int:
+        return ValueError('salary variable must be an integer')
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
