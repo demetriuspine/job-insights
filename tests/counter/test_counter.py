393c86562d0e95fa3a967f189c5d48affca60df3
lucas <mr.pine97@gmail.com>
2022-07-04 17:11:07 -0300

    req10 - feat: implementa o teste de count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('src/jobs.csv', 'year') == 5431
+    assert count_ocurrences('src/jobs.csv', 'Year') == 5431
+    assert count_ocurrences('src/jobs.csv', 'demetrius') == 0
+
+
